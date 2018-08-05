839c7b6699b76085d4baf4837ddbca391d0d0289
chaos2007 <chaos2007@gmail.com>
2018-08-05 16:37:02 -0500

    Adding Icons to lines

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,9 @@ class MyApp extends StatelessWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-
+  final Set<WordPair> _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
+
 
   Widget _buildSuggestions() {
     return ListView.builder(
@@ -47,10 +48,15 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: new Icon(   // Add the lines from here...
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
     );
   }
