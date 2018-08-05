54612f8e70f90490312aaeb7a5c3b89a50422a28
chaos2007 <chaos2007@gmail.com>
2018-08-05 17:00:11 -0500

    Change theme style

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: new ThemeData(          // Add the 3 lines from here...
+        primaryColor: Colors.red,
+      ),
       home: RandomWords(),
     );
   }
